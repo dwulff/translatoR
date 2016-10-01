01b8c687ba9178310d6dc18ce3ed08c57f53bd4d
dwulff <dirk.wulff@gmail.com>
2016-10-02 00:51:34 +0200

    fixed comma issue in strsplit

--- R/translatoR.R
+++ R/translatoR.R
@@ -16,7 +16,7 @@ translatoR = function(text, source, target){
   url = paste0("https://translate.googleapis.com/translate_a/single?client=gtx&sl=",
                source,"&tl=",target,"&dt=t&q=",txt)
   res = RCurl:::getURL(url)
-  tra = strsplit(res,',')[[1]][1]
+  tra = strsplit(res,'",')[[1]][1]
   tra = substr(tra,5,nchar(tra)-1)
   return(tra)
   }
