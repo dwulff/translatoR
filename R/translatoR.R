47ef38a42c1f6dc98c18ffe776b27b7474ae5eb9
dwulff <dirk.wulff@gmail.com>
2016-10-01 23:43:05 +0200

    first commit

--- R/translatoR.R
+++ R/translatoR.R
@@ -1,11 +1,9 @@
-require(utils)
-require(RCurl)
 
 translatoR = function(text, source, target){
-  txt = utils::URLencode(text)
+  txt = utils:::URLencode(text)
   url = paste0("https://translate.googleapis.com/translate_a/single?client=gtx&sl=",
                source,"&tl=",target,"&dt=t&q=",txt)
-  res = RCurl::getURL(url)
+  res = RCurl:::getURL(url)
   tra = strsplit(res,',')[[1]][1]
   tra = substr(tra,5,nchar(tra)-1)
   return(tra)
