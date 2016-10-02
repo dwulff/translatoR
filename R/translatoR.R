cf2a9a1fc3af30a4c455f0a3dcafca283849082b
dwulff <dirk.wulff@gmail.com>
2016-10-02 02:18:12 +0200

    enabled vector input

--- R/translatoR.R
+++ R/translatoR.R
@@ -12,14 +12,22 @@
 #' @author Dirk U. Wulff
 
 translatoR = function(text, source, target){
+  characters = c(as.character(0:9),letters,LETTERS)
+  collapse   = paste0(' ',paste(sample(characters,100,replace = T),collapse=''),' ')
+  if(length(text)>1) text = paste(text,collapse=collapse)
+  
   txt = utils:::URLencode(text)
   url = paste0("https://translate.googleapis.com/translate_a/single?client=gtx&sl=",
                source,"&tl=",target,"&dt=t&q=",txt)
   res = RCurl:::getURL(url)
-  tra = strsplit(res,'",')[[1]][1]
-  tra = substr(tra,5,nchar(tra)-1)
+  res = strsplit(res,'\\],\\[')[[1]]
+  tra = strsplit(res,'",')
+  tra = unlist(lapply(tra,function(x) x[1]))
+  tra = gsub('\\[\\[\\[\\"','',tra)
+  tra = gsub('\"','',tra)
+  tra = paste(tra,collapse='')
+  tra = strsplit(tra,collapse)[[1]]
   return(tra)
   }
-
 
 
